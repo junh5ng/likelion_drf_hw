604ac9c3ebc15829b6b08bf2d6fdbe0644901c16
junh5ng <wnsghd1130@dgu.ac.kr>
2025-07-15 18:10:49 +0900

    이미지 처리를 위한 초기 세팅 설정

--- project/settings.py
+++ project/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+from pathlib import Path
+import os
+
+STATIC_URL='static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
